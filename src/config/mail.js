3df4d30104050a1c39abce59db52a618229e1602
Gabriel <gabriel.capila20@gmail.com>
2020-09-21 16:53:11 -0300

    commit

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f936b63b5dd970',
+    pass: '967e37981d84a0',
+  },
+  default: {
+    from: 'Team Gobarber <noreply@gobarber.com>',
+  },
+};
